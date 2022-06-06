d81a94da897de1fc462e0f7b1abe188ff9f37cc0
MinhTranLe2010 <91017310+MinhTranLe2010@users.noreply.github.com>
2022-06-06 08:05:53 +0700

    Add files via upload

---
+++ admin/includes/leftbar.php
@@ -0,0 +1,31 @@
+﻿	<nav class="ts-sidebar">
+			<ul class="ts-sidebar-menu">
+			
+				<li class="ts-label">Main</li>
+				<li><a href="dashboard.php"><i class="fa fa-dashboard"></i> Bảng điều khiển</a></li>
+			
+<li><a href="#"><i class="fa fa-files-o"></i> Thương hiệu</a>
+<ul>
+<li><a href="create-brand.php">Khởi tạo thương hiệu</a></li>
+<li><a href="manage-brands.php">Quản lý thương hiệu</a></li>
+</ul>
+</li>
+
+<li><a href="#"><i class="fa fa-sitemap"></i> Xe</a>
+					<ul>
+						<li><a href="post-avehical.php">Thêm xe</a></li>
+						<li><a href="manage-vehicles.php">Quản lý xe</a></li>
+					</ul>
+				</li>
+				<li><a href="manage-bookings.php"><i class="fa fa-users"></i> Quản lý đặt xe</a></li>
+
+				<li><a href="testimonials.php"><i class="fa fa-table"></i> Quản lý xác thực</a></li>
+				<li><a href="manage-conactusquery.php"><i class="fa fa-desktop""></i> Quản lý truy vấn</a></li>
+				<li><a href="reg-users.php"><i class="fa fa-users"></i> Quản lý người thuê xe</a></li>
+			<li><a href="manage-pages.php"><i class="fa fa-files-o"></i>Quản lý trang</a></li>
+			<li><a href="update-contactinfo.php"><i class="fa fa-files-o"></i> Cập nhật thông tin liên hệ</a></li>
+
+			<li><a href="manage-subscribers.php"><i class="fa fa-table"></i> Quản lý người theo dõi</a></li>
+
+			</ul>
+		</nav>
